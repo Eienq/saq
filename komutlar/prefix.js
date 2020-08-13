a908c09716874cef245c0b08d867b82e5585ae81
Glitch (fynx-v12muzikbotu) <none>
2020-08-13 14:08:56 +0000

    🍮🔖 Checkpoint ./komutlar/prefix.js:2344545/1258

--- komutlar/prefix.js
+++ komutlar/prefix.js
@@ -19,27 +19,35 @@ if(args[0] === "ayarla") {
 if(o) { return message.channel.send(new Discord.MessageEmbed()
 .setColor(FynxHata)
 .setTitle("Fynx Music - Hata")
-.setDescription(`Prefix zaten ayarlanmış.\nFynx Music'in şuan ki  prefixi: \`${p}\` \nFynx Music'in sıfırlamak için \`${p}prefix sıfırla\` yazabilirsiniz. `));               
+.setDescription(`Fynx Music'in prefixi zaten ayarlanmış.\nFynx Music'in şuan ki  prefixi: \`${p}\` \nFynx Music'in sıfırlamak için \`${p}prefix sıfırla\` yazabilirsiniz. `));               
       }
 if(!args[1]) return message.channel.send(new Discord.MessageEmbed()
 .setColor(FynxHata)
 .setTitle("Fynx Music - Hata")
-.setDescription(`Bir Prefix Girip Tekrar Dene. Şuanki Prefix: ${p}`));
+.setDescription(`Lütfen bir prefix giriniz.\nFynx Music'in şu anki prefixi: \`${p}\``));
 db.set(`prefix.${message.guild.id}`, args[1])
 message.channel.send(new Discord.MessageEmbed()
-.setDescription(`Prefix Başarıyla Ayarlandı. Şuanki Prefix: ${args[1]}`));
+.setColor(FynxDogru)
+.setTitle("Fynx Music - Başarılı")
+.setDescription(`Fynx Music'in yeni prefixi ayarlandı.\nYeni prefix: \`${args[1]}\``));
 }
     if(args[0] === "sıfırla") {
     if(!o) {
        return message.channel.send(new Discord.MessageEmbed()
-.setDescription(`Ayarlanmayan Prefixi Sıfırlayamazsınız. Şuanki Prefix: ${p}`));
+.setColor(FynxHata)
+.setTitle("Fynx Music - Hata")
+.setDescription(`Fynx Music'in prefixini sıfırlayabilmek için önceden ayarlı olması gerekmektedir.\nFynx Music'in şu anki prefixi: \`${p}\``));
     }
     db.delete(`prefix.${message.guild.id}`)       
    return message.channel.send(new Discord.MessageEmbed()
-.setDescription(`Prefix Başarıyla Sıfırlandı. Şuanki Prefix: ${a}`));
+.setColor(FynxDogru)
+.setTitle("Fynx Music - Başarılı")
+.setDescription(`Fynx Music'in prefixi başarılı bir şekilde sıfırlandı.\nFynx Music'in şu anki prefixi: \`${a}\``));
   }
  
- if(!args[0]) return message.channel.send(new Discord.MessageEmbed()        
+ if(!args[0]) return message.channel.send(new Discord.MessageEmbed()
+.setColor(FynxHata)
+.setTitle("Fynx Music - Hata")
 .setDescription(`Prefix Ayarlamak İçin ${p}prefix ayarla <prefix> / Sıfırlamak İçin ${p}prefix sıfırla Şuanki Prefix: ${p}`));
   
 };
