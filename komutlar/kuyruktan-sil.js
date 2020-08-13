7dbf20bcc2e8e19a59045fc1a4641d9f2ec28b15
Glitch (fynx-v12muzikbotu) <none>
2020-08-13 18:55:53 +0000

    🎩💆 Checkpoint ./komutlar/kuyruk.js:2344545/50 ./komutlar/kuyruktan-sil.js:2344545/885 ./ayarlar/bot.json:2344545/2

--- komutlar/kuyruktan-sil.js
+++ komutlar/kuyruktan-sil.js
@@ -5,14 +5,17 @@ const FynxDogru = "#22BF41";
 const FynxHata = "#f30707";
 
 module.exports.run = async (client, message, args) => {
+
 if(!client.player.isPlaying(message.guild.id)) return message.channel.send({embed: {color: FynxHata, description: `<a:yanlis:734892943332212764>  | Şu anda hiçbir müzik çalmamaktadır!` }})
-const track = args[0];
-if (!track) return message.channel.send({embed: {color: FynxHata, description: `<a:yanlis:734892943332212764>  | Kuyruğu silebilmek için sadece bir doğal sayı giriniz.` }})
+if (!args[0]) return message.channel.send({embed: {color: FynxHata, description: `<a:yanlis:734892943332212764>  | Kuyruğu silebilmek için sadece bir doğal sayı giriniz.` }})
 if (args > 10) return message.channel.send({embed: {color: FynxHata, description: `Kuyruğu silebilmek için sadece \`1\` ile \`10\` arasında bir değer giriniz. `}})
 if (args < 1) return message.channel.send({embed: {color: FynxHata, description: `Kuyruğu silebilmek için sadece \`1\` ile \`10\` arasında bir değer giriniz. ` }})
 if(!message.member.voice.channel) return message.channel.send({embed: {color: FynxHata, description: `<a:yanlis:734892943332212764>  | Kuyruğu silebilmek için bir ses kanalında olmanız gerekmektedir!` }});
-client.player.remove(message.guild.id, track);
-message.channel.send({embed: {color: FynxDogru, description: `<a:tik:734892939737694239>  | Kuyruktaki ${track} numaralı müzik kuyruktan silindi!` }})
+const kuyruk = client.player.getQueue(message.guild.id);
+let q = kuyruk.songs.map((song, i) => {
+return   client.player.remove(message.guild.id, i);
+  message.channel.send({embed: {color: FynxDogru, description: `<a:tik:734892939737694239>  | Kuyruktaki ${args[0]} numaralı müzik kuyruktan silindi!` }})
+})
 };
 
 module.exports.config = {
