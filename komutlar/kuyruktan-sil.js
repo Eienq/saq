8f25b9a77d8de73f576e16320e5f7be6746455ac
Glitch (fynx-v12muzikbotu) <none>
2020-08-13 18:43:52 +0000

    🎯🚘 Checkpoint ./komutlar/kuyruktan-sil.js:2344545/1393

--- komutlar/kuyruktan-sil.js
+++ komutlar/kuyruktan-sil.js
@@ -4,15 +4,18 @@ const Fynx = "#36393e";
 const FynxDogru = "#22BF41";
 const FynxHata = "#f30707";
 
-module.exports.run = async (client, message, args, guild) => {
-  const track = parseInt(args.join(" "));
-if(!message.member.voice.channel) return message.channel.send({embed: {color: FynxHata, description: `<a:yanlis:734892943332212764>  | Kuyruğu temizleyebilmek için bir ses kanalında olmanız gerekmektedir!` }});
+module.exports.run = async (client, message, args) => {
 if(!client.player.isPlaying(message.guild.id)) return message.channel.send({embed: {color: FynxHata, description: `<a:yanlis:734892943332212764>  | Şu anda hiçbir müzik çalmamaktadır!` }})
+const track = args[0];
+if (!track) return message.channel.send({embed: {color: FynxHata, description: `<a:yanlis:734892943332212764>  | Kuyruğu silebilmek için sadece bir doğal sayı giriniz.` }})
+if (args > 10) return message.channel.send({embed: {color: FynxHata, description: `Kuyruğu silebilmek için sadece \`1\` ile \`10\` arasında bir değer giriniz. `}})
+if (args < 1) return message.channel.send({embed: {color: FynxHata, description: `Kuyruğu silebilmek için sadece \`1\` ile \`10\` arasında bir değer giriniz. ` }})
+if(!message.member.voice.channel) return message.channel.send({embed: {color: FynxHata, description: `<a:yanlis:734892943332212764>  | Kuyruğu silebilmek için bir ses kanalında olmanız gerekmektedir!` }});
 client.player.remove(message.guild.id, track);
-message.channel.send({embed: {color: FynxDogru, description: `<a:tik:734892939737694239>  | Kuyruk başarılı bir şekilde temizlendi!` }})
+message.channel.send({embed: {color: FynxDogru, description: `<a:tik:734892939737694239>  | Kuyruktaki ${track} numaralı müzik kuyruktan silindi!` }})
 };
 
 module.exports.config = {
-    name: "kuyruğu-temizle",
-    aliases: ["kuyruk-temizle"]
+    name: "kuyruktan-sil",
+    aliases: ["sil"]
 };
