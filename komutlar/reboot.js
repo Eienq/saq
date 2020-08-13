cf3fd50cac11b5c5deeaad337aa53e2fb3f16f89
Glitch (fynx-v12muzikbotu) <none>
2020-08-13 19:18:53 +0000

    🎙🛩 Checkpoint ./komutlar/reboot.js:2344545/461

--- komutlar/reboot.js
+++ komutlar/reboot.js
@@ -1,16 +1,20 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 const ayarlar = require("../ayarlar.json")
+const Fynx = "#36393e";
+const FynxDogru = "#22BF41";
+const FynxHata = "#f30707";  
+
 
 module.exports.run = async (bot, message, args) => { 
    
   var embed2 = new Discord.RichEmbed()   
-      .setTitle('Merhaba, ' + message.member.user.username)
-      .setDescription('sadece sahibim bu komutu  kullanabilir! Yani Pudochu `R#0001 ')
+      .setTitle('Fynx Music - Hata')
+      .setDescription('Bu komutu kullanabilmek için bot yapımcısı olman gerekmektedir.\nBot yapımcıları; <@327064201245753344> <@236173144300191754>')
       .setColor('RED') 
   
 
-  if(message.author.id !== "450006793884663820") return message.channel.sendEmbed(embed2)
+  if(message.author.id !== ["327064201245753344", "236173144300191754"]) return message.channel.sendEmbed(embed2)
      
   //Cortex botun reboot sistemi hadi h.o
   var embed = new Discord.RichEmbed()   
