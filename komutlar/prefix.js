0e8d8a9104ad22d729dd4c0f321616010495724d
Glitch (fynx-v12muzikbotu) <none>
2020-08-13 13:56:55 +0000

    🛥⛄️ Checkpoint ./komutlar/prefix.js:2344545/960

--- komutlar/prefix.js
+++ komutlar/prefix.js
@@ -2,17 +2,28 @@ const Discord = require('discord.js')
 const db = require('quick.db');
 const ayarlar = require('../ayarlar/bot.json')
 exports.run = async (client, message, args) => {
+const Fynx = "#36393e";
+const FynxDogru = "#22BF41";
+const FynxHata = "#f30707";  
+  
 let a = ayarlar.prefix
     let p = await db.fetch(`prefix.${message.guild.id}`) || ayarlar.prefix
  let o = await db.fetch(`prefix.${message.guild.id}`)
+ 
   if(!message.member.hasPermission("MANAGE_GUILD")) return message.channel.send(new Discord.MessageEmbed()
-.setDescription(`Bu Komutu Kullanabilmek İçin Sunucuyu Yönet Yetkisine Sahip Olmalısınız. Fynx Music'in Şuan kPrefixi: ${p}`));
+.setColor(FynxHata)
+.setTitle("Fynx Music - Hata")
+.setDescription(`Bu komutu kullanabilmek için;\n**Sunucuyu Yönet** yetkisine sahip olmanız gerekmektedir. \nFynx Music'in şu anki prefixi: \`${p}\``));
   
 if(args[0] === "ayarla") {
 if(o) { return message.channel.send(new Discord.MessageEmbed()
-.setDescription(`Ayarlanmış Şeyi Tekrar Ayarlıyamassın. Şuanki Prefix: ${p} Sıfırlamak İçin ${p}prefix sıfırla`));
+.setColor(FynxHata)
+.setTitle("Fynx Music - Hata")
+.setDescription(`Prefix zaten ayarlanmış.\nFynx Music'in şuan ki  prefixi: \`${p}\` \nFynx Music'in sıfırlamak için \`${p}prefix sıfırla\` yazabilirsiniz. `));               
       }
 if(!args[1]) return message.channel.send(new Discord.MessageEmbed()
+.setColor(FynxHata)
+.setTitle("Fynx Music - Hata")
 .setDescription(`Bir Prefix Girip Tekrar Dene. Şuanki Prefix: ${p}`));
 db.set(`prefix.${message.guild.id}`, args[1])
 message.channel.send(new Discord.MessageEmbed()
