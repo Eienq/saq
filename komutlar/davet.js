111cf757a0e27bdee7d32ca467bda4493c34895b
Glitch (fynx-v12muzikbotu) <none>
2020-08-14 22:20:16 +0000

    👰🌒 Checkpoint ./komutlar/davet.js:2344545/416

--- komutlar/davet.js
+++ komutlar/davet.js
@@ -5,7 +5,7 @@ exports.run = async (client, message, args) => {
 const embed = new Discord.MessageEmbed()
 .setColor("BLUE")
 .setAuthor(`Fynx Music Linkler`, client.user.avatarURL())
-.setDescription('**• [FYNX](https://discord.gg/asCQAEA)**\n\n**• [FynxCode](https://discord.gg/fynxcode)**\n\n**• [FynxStore](https://discord.gg/pc74FNX)**\n\n**• [Yapımcı İnstagram Hesabı](https://instagram.com/m_arda_dusova)**')
+.setDescription('**• [Fynx Music`i Ekleyin.](https://discord.com/oauth2/authorize?client_id=522870338867167254&scope=bot&permissions=8)**\n\n**• [Fynx Music Websitesi](https://fynxmusic.tk/)**')
 .setFooter(`${message.author.username} tarafından istendi!`) 
 .setTimestamp()
 .setThumbnail('https://cdn.discordapp.com/app-icons/522870338867167254/c82cd947b45d9d3a0f34ba8aaf0422ee.png')
