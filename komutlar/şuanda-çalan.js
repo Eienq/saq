db85a2d6a28a0eadaecd121d65ce3ae117dc9936
Glitch (fynxmusicbots) <none>
2020-08-21 18:50:45 +0000

    ☕️🐉 Checkpoint ./komutlar/şuanda-çalan.js:2344545/358 ./package.json:2344545/10

--- komutlar/şuanda-çalan.js
+++ komutlar/şuanda-çalan.js
@@ -21,8 +21,8 @@ const calan = await client.player.nowPlaying(message.guild.id);
 const calanembed = new Discord.MessageEmbed()
 .setThumbnail(calan.thumbnail)
 .setColor("#22BF41")
-.setDescription(`<a:calan:735111831550427166>  | Şu Anda Oynatılan:\n\nMüziğin Adı: \n\`${calan.name}\`\n\nMüziği Yükleyen Kanal: \n\`${calan.author}\` \n\nMüziğin Linki: \n\`${calan.url}\``)
-.setFooter(`${client.user.name}`)
+.setDescription(`<a:calan:735111831550427166>  | Şu Anda Oynatılan:\n\nMüziğin Adı: \n\`${calan.name}\`\n\nMüziği Yükleyen Kanal: \n\`${calan.author}\` \n\nMüziğin Durumu: \n\`${client.player.createProgressBar(message.guild.id)}\``)
+.setFooter(`[${client.player.createProgressBar(message.guild.id)}] [${calan.durationMS}]`)
 message.channel.send(calanembed)
 };
 
