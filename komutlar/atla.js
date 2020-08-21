e8958e95b4d670b3ea5a3df905fbbcd531e440c8
Glitch (fynxmusicbots) <none>
2020-08-21 21:07:42 +0000

    👚✌️ Checkpoint ./komutlar/atla.js:2344545/1430 ./komutlar/yardım.js:2344545/389

--- komutlar/atla.js
+++ komutlar/atla.js
@@ -9,34 +9,6 @@ const FynxHata = "#f30707";
 module.exports.run = async (client, message) => {
   	let p = db.fetch(`prefix.${message.guild.id}`) || ayarlar.prefix;
 
-//const hataembed = new Discord.MessageEmbed()
-//.setColor(FynxHata)
-//.setTitle("Fynx Music - Hata")
-//.setDescription(`Lütfen bir dil seçiniz.\nMevcut diller; \`TR\`, \`EN\`\nKullanım: \`${p}dil TR\``)
-
-//const errorembed = new Discord.MessageEmbed()
-//.setColor(FynxHata)
-//.setTitle("Fynx Music - Hata")
-//.setDescription(`Please select a language.\nAvailable languages; \`TR\`, \`EN\`\nUsage: \`{p}language EN\` `)
-
-  		//let aktif = db.has(`lang_${message.guild.id}`, 'TR')
-//if(!aktif) return message.channel.send(hataembed) 
-  
-    		//let enable = db.has(`lang_${message.guild.id}`, 'EN')
-//if(!enable) return message.channel.send(errorembed) 
-
- // if(db.has(`lang_${message.guild.id}`, 'EN')) {
-  //if(!message.member.voice.channel) return message.channel.send({embed: {color: FynxHata, description: `<a:yanlis:734892943332212764>  | You must be on an audio channel to skip a playing music!` }})
-//if(!client.player.isPlaying(message.guild.id)) return message.channel.send({embed: {color: FynxHata, description: `<a:yanlis:734892943332212764>  | No music is currently playing!` }})
-//const song = await client.player.skip(message.guild.id);
-//message.channel.send({embed: {color: FynxDogru, description: `<a:tik:734892939737694239>  | Skipped Music:\n\`${song.name}\`` }})
-
-
-//}
-
-////////////// TÜ RK ÇE //////////////////
-
-  //if(db.has(`lang_${message.guild.id}`, 'TR')) {
 if(!message.member.voice.channel) return message.channel.send({embed: {color: FynxHata, description: `<a:yanlis:734892943332212764>  | Oynatılan bir müziği atlayabilmek için bir ses kanalında olmanız gerekmektedir!` }})
 if(!client.player.isPlaying(message.guild.id)) return message.channel.send({embed: {color: FynxHata, description: `<a:yanlis:734892943332212764>  | Şu anda hiçbir müzik çalmamaktadır!` }})
 const sarki = await client.player.skip(message.guild.id);
@@ -48,7 +20,6 @@ const embed = new Discord.MessageEmbed()
 .setFooter(`Fynx Music - Tüm hakları saklıdır.`, client.user.avatarURL())
 message.channel.send(embed)
 
-  ////////////////////////////////////////
 };
 
 module.exports.config = {
--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -12,7 +12,7 @@ let pages = [
 "**Fynx Music kullanıcılarının gizliliği;**\n\n<:guvenlik:735597569597767840>  Biz Fynx Music geliştiricileri olarak sizin gizliliğinizi önemsiyor ve saygı duyuyoruz. Bu sebepten ötürü internet sayfamızda **Gizlilik** adlı bir konu açtık.\n\n**`Detaylı Bilgi:`**\n<a:secenek:733987076718198864> https://fynxmusic.tk/gizlilik",          
 `**Fynx Music kullanıcı komutları**\n\n` + `\`${prefix}yardım\`` + `\n<a:secenek:733987076718198864>  Bütün komutları ve açıklamaları gösterir. \n\n` + `\`${prefix}oynat <Şarkı İsmi>\`` +`\n<a:secenek:733987076718198864>  Belirtilen isimli şarkıları arar ve bulunduğunuz odaya bağlanıp müziği oynatır. \n\n` + `\`${prefix}durdur\`` +`\n<a:secenek:733987076718198864>  Fynx Music müzik oynatmayı durdurur ve ses kanalından ayrılır.\n\n ` + `\`${prefix}atla\`` + `\n<a:secenek:733987076718198864>  Oynatılmakta olan müziği atlar. Kuyrukta müzik var ise müzikler sırayla oynatılır. \n\n ` + `\`${prefix}duraklat\`` +` \n<a:secenek:733987076718198864>  Oynatılan olan müziği duraklatır. \n\n ` + `\`${prefix}devam\`` +`\n<a:secenek:733987076718198864>  Duraklatılan müziği devam ettirir.`,
 `**Fynx Music kullanıcı komutları**\n\n` + `\`${prefix}karıştır\`` +`\n<a:secenek:733987076718198864>  Müzik kuyruğundaki müzikleri karıştırır. \n\n ` + `\`${prefix}döngü\`` +`\n<a:secenek:733987076718198864>  Müzik kuyruğundaki müzikleri döngü içerisine alır. \n\n ` + `\`${prefix}çalan\`` +`\n<a:secenek:733987076718198864>  Oynatılan olan müziği gösterir.\n\n  ` + `\`${prefix}kuyruk\`` +`\n<a:secenek:733987076718198864>  Müzik kuyruğunu gösterir. \n\n ` + `\`${prefix}kuyruğu-temizle\`` +`\n<a:secenek:733987076718198864>  Müzik kuyruğunu temizler.\n\n ` + `\`${prefix}ses <1/200>\`` +`\n<a:secenek:733987076718198864>  Oynatılan müziğin ses seviyesini ayarlar.`,
-'**Fynx Music Linkler**\n\n**• [FYNX](https://discord.gg/asCQAEA)**\n\n**• [FynxCode](https://discord.gg/fynxcode)**\n\n**• [FynxStore](https://discord.gg/pc74FNX)**\n\n**• [Yapımcı İnstagram Hesabı](https://instagram.com/m_arda_dusova)**'
+'**Fynx Music Linkler**\n\n**• [Fynx Music`i Ekleyin.](https://discord.com/oauth2/authorize?client_id=522870338867167254&scope=bot&permissions=8)**\n\n**• [Fynx Music Websitesi](https://fynxmusic.tk/)**\n\n**• [Yapımcı İnstagram Hesabı](https://instagram.com/m_arda_dusova)**'
 ];
 let page = 1; 
 const embed = new Discord.MessageEmbed()
@@ -24,9 +24,9 @@ const embed = new Discord.MessageEmbed()
 message.channel.send(embed).then(msg => {
 msg.react('⬅')
 .then(r => {
-msg.react('➡')
+msg.react(':heavy_plus_sign:')
 const backwardsFilter = (reaction, user) => reaction.emoji.name === '⬅' && user.id === message.author.id;
-const forwardsFilter = (reaction, user) => reaction.emoji.name === '➡' && user.id === message.author.id;
+const forwardsFilter = (reaction, user) => reaction.emoji.name === ':heavy_plus_sign:' && user.id === message.author.id;
 const backwards = msg.createReactionCollector(backwardsFilter, { time: 100000 });
 const forwards = msg.createReactionCollector(forwardsFilter, { time: 100000 });
 forwards.on('collect', r => {
