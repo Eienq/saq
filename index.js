0e148e02302ab1dbd19e00035b249df86d791691
Glitch (fynxmusicbots) <none>
2020-08-21 22:50:50 +0000

    🚜🌥 Checkpoint ./index.js:2344545/106

--- index.js
+++ index.js
@@ -43,12 +43,12 @@ defaultChannel.send(emmmmbed)
 //----------------------------------------------------------------\\
 const http = require('http'); 
 app.get("/", (request, response) => { 
-  console.log(` pingleme işlemi başarılı başarılıysa bu yazıyı loglarda görürsün`); 
+  console.log(fynx.pingmesaj); 
   response.sendStatus(200); 
 }); 
 app.listen(process.env.PORT); 
 setInterval(() => { 
-  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`); 
+  http.get(`http://fynxmusicbots.glitch.me/`); 
 }, 280000);
 
 const player = new Player(client, fynx.youtube_api);
