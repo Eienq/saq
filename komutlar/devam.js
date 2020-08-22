66fa8293efece308996cb7ef883240998f57eed7
Glitch (fynxmusicbots) <none>
2020-08-22 18:14:53 +0000

    📡🌇 Checkpoint ./komutlar/atla.js:2344545/2376 ./komutlar/devam.js:2344545/1161

--- komutlar/devam.js
+++ komutlar/devam.js
@@ -7,7 +7,31 @@ const db = require("quick.db");
 const ayarlar = require("../ayarlar/bot.json");
 
 module.exports.run = async (client, message) => {
-  	let p = db.fetch(`prefix.${message.guild.id}`) || ayarlar.prefix;
+
+const sarki = await client.player.resume(message.guild.id);
+  
+let p = db.fetch(`prefix.${message.guild.id}`) || ayarlar.prefix;
+  
+ //------------------------------------------------//
+
+const hata1 = new Discord.MessageEmbed()
+.setColor(FynxHata) 
+.setTitle("• Hata: 001 •")
+.setThumbnail(message.author.avatarURL())
+.setDescription(`<a:yanlis:734892943332212764>  | Oynatılan bir müziği geçebilmek için bir ses kanalında olmanız gerekmektedir!`)
+.setFooter(`Fynx Music - Tüm hakları saklıdır.`, client.user.avatarURL());  
+if(!message.member.voice.channel) return message.channel.send(hata1)
+
+//------------------------------------------------//
+  
+const hata2 = new Discord.MessageEmbed()
+.setColor(FynxHata) 
+.setTitle("• Hata: 002 •")
+.setThumbnail(message.author.avatarURL())
+.setDescription(`<a:yanlis:734892943332212764>  | Şu anda hiçbir müzik çalmamaktadır!`)
+.setFooter(`Fynx Music - Tüm hakları saklıdır.`, client.user.avatarURL());    
+if(!sarki) return message.channel.send(hata2)
+//------------------------------------------------// 
 
 if(!message.member.voice.channel) return message.channel.send({embed: {color: FynxHata, description: `<a:yanlis:734892943332212764>  | Duraklatılan bir müziği devam ettirebilmek için bir ses kanalında olmanız gerekmektedir!` }})
 const sarki = await client.player.resume(message.guild.id);
