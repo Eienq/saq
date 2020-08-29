66c029a4027aad9786d67a3aa3dca03db0e1277b
Glitch (fynxmusicbots) <none>
2020-08-29 16:00:07 +0000

    🏤💡 Checkpoint ./komutlar/davet.js:2344545/36

--- komutlar/davet.js
+++ komutlar/davet.js
@@ -9,7 +9,7 @@ exports.run = async (client, message, args) => {
 const embed = new Discord.MessageEmbed()
 .setColor("BLUE")
 .setAuthor(`Alone Music Linkler`, client.user.avatarURL())
-.setDescription('**• [Alone Music`i Ekleyin.](https://discord.com/oauth2/authorize?client_id=522870338867167254&scope=bot&permissions=8)**\n\n**• [Alone Music Websitesi](https://alonemusicbot.tk/)**')
+.setDescription('**• [Alone Music`i Ekleyin.](https://discord.com/oauth2/authorize?client_id=511593657711722523&scope=bot&permissions=8)**\n\n**• [Alone Music Websitesi](https://alonemusicbot.tk/)**')
 .setFooter(`${message.author.username} tarafından istendi!`) 
 .setTimestamp()
 .setThumbnail('https://cdn.discordapp.com/app-icons/522870338867167254/c82cd947b45d9d3a0f34ba8aaf0422ee.png')
