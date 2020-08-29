49240a54cf06cabb463890367f1676f7745936e0
Glitch (fynxmusicbots) <none>
2020-08-29 09:51:09 +0000

    🏂🎪 Checkpoint ./dilayarla.js:2344545/1563

--- dilayarla.js
+++ dilayarla.js
@@ -1,10 +1,23 @@
+const Discord = require("discord.js")
+const fs = require("fs")
+const Fynx = "#36393e";
+const FynxDogru = "#22BF41";
+const FynxHata = "#f30707";
 const db = require("quick.db")
 const ayarlar = require("../ayarlar/bot.json")
 exports.run = async function(client, message, args) {
+
+  const seceneksiz = new Discord.MessageEmbed()
+  .setColor(FynxHata)
+  .setTitle("• Hata: 005 •")
+.setThumbnail(message.author.avatarURL())
+.setDescription(`<a:yanlis:734892943332212764>  | Fynx Music'in dilini ayarlayabilmek için bir dil belirtiniz.\nMevcut Diller: \`Türkçe\`, \`English\`To be able to set the language of Fynx Music, you must specify a language. Available languages:`)
+.setFooter(`Fynx Music - Tüm hakları saklıdır.`, client.user.avatarURL());  
+  
 let dil = await db.fetch(`dil_${message.guild.id}`);
 const secenek = args[0]
   if (!message.member.hasPermission("MANAGE_GUILD")) return message.channel.send(`/**SUNUCUYU-YONET**/ yetkin yok.`)
-if(!secenek) return message.channel.send(ayarlar.prefix+'dilayarla tr yada en şeklinde kullanın')
+if(!secenek) return message.channel.send(seceneksiz)
 
 if(secenek == 'tr' || "TR" || "Turkish" || "Türk"|| "türk" || "türkçe" || "Türkçe"){
 db.set(`dil_${message.guild.id}`, 'TR')
