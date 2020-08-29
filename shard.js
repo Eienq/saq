b136d265773b5a21471501465eae26060e4e2aac
Glitch (fynxmusicbots) <none>
2020-08-29 08:03:03 +0000

    📼🔖 Checkpoint ./komutlar/shard.js:2344545/1745 ./shard.js:2344545/671 ./package.json:2344545/20

---
+++ shard.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js');
+const fynx = require("./ayarlar/bot.json");
+const shard= new Discord.ShardingManager('./index.js', { // main dosyanızın ismi: server.js - main.js - bot.js olabilir
+    totalShards: 20, // shard sayısı ya da auto yazılabilir // 2k veya 1k sunucularda ideali: 2'dir.
+    token: fynx.fynxtoken // token
+});
+
+shard.spawn(); // S2Ş ahanda burada başlıyor
+
+shard.on('launch', shard => {
+  console.log(`${shard.id} IDli shard başarıyla başlatıldı .`)
+});
+
+setTimeout(() => {
+    console.log("yeniden başlatılıyor..")
+    shard.broadcastEval("process.exit()");
+}, 21600000);
