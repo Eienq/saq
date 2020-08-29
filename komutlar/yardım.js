70fa4e2d689719150e809676e2f4dd184793a0b5
Glitch (fynxmusicbots) <none>
2020-08-29 22:19:54 +0000

    🐥🚉 Checkpoint ./komutlar/yardım.js:20905456/1427 ./komutlar/prefix.js:20905456/1725

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -2,16 +2,12 @@ const Discord = require('discord.js');
 exports.run = async (client, message, args) => { 
 let yardım = new Discord.MessageEmbed()  
 .setAuthor(`Pirate Yardım Menüsü`)
-.setColor('#ffcc00')
-.addField('Pirate Botunu Eklemek İsterseniz -davet',`
-<a:pirate:749380925619437619> ** = \`-genel\` : Genel Komutlar**
-<a:pirate:749380925619437619> **= \`-koruma\` : Sunucu Koruma**
-<a:pirate:749380925619437619> ** = \`-ayarlar\` : Moderasyon Komutları**
-<a:pirate:749380925619437619> ** = \`-sunucu\` : Sunucu Komutları**
-<a:pirate:749380925619437619> **= \`-eğlence\` : Eğlence Komutları**
-<a:pirate:749380925619437619> **= \`-eklenti\` : Eklenti Komutları**
-<a:pirate:749380925619437619> ** = \`-logo\` : Logo Generator**`)
-.addField('Pirate Bot',`  [Botumuza Oy Ver](https://top.gg/bot/713713727794446397) **|**  [Botu Davet Et](https://discord.com/oauth2/authorize?client_id=713713727794446397&scope=bot&permissions=8) **|** [Destek](https://discord.gg/HRC9Yyp)`)
+.setColor('#ffd100')
+.addField('Sunucununuz Artık  Pirate ile 7/24 Güvenli',`
+<a:pirate:749380925619437619> ** | **__**-SUNUCU**__ **| Sunucu Komutları**
+<a:pirate:749380925619437619> ** | **__**-SUNUCU**__ **| Sunucu Komutları**
+<a:pirate:749380925619437619> ** | **__**-SUNUCU**__ **| Sunucu Komutları**`)
+.addField('<a:pirate:749380925619437619> | Pirate Bot',`  [Botumuza Oy Ver](https://top.gg/bot/713713727794446397) **|**  [Botu Davet Et](https://discord.com/oauth2/authorize?client_id=713713727794446397&scope=bot&permissions=8) **|** [Destek](https://discord.gg/HRC9Yyp)`)
 .setImage(`https://i.hizliresim.com/4Q3agT.jpg`)
 .setThumbnail(client.user.avatarURL)
  message.channel.send(yardım) 
