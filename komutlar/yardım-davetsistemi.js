2b91c25db11462e2cec2c6660bc0bc429bca2a5d
Glitch (mint-trite-buckthorn) <none>
2020-08-30 22:34:51 +0000

    🌦🏀 Checkpoint ./komutlar/yardım-otocevap.js:2344545/3492 ./komutlar/yardım-davetsistemi.js:2344545/2858 ./komutlar/yardım-kayıtsistemi.js:2344545/3485

--- komutlar/yardım-davetsistemi.js
+++ komutlar/yardım-davetsistemi.js
@@ -7,9 +7,12 @@ let eklenti = new Discord.MessageEmbed()
 .setAuthor(`Pirate Yardım Menüsü`)
 .setColor('#ffd100')
 .addField('Sunuzunuzu 7/24 Denetleyin',`
-<a:pirate:749380925619437619> ** | **__**${prefix}SAYAÇ-AYARLA**__ **| Sunucunuzda Sayaç Ayarlar.**
-<a:pirate:749380925619437619> ** | **__**${prefix}SAYAÇ-HG-MESAJ**__ **| Sunucunuzdaki Sayacın Hoş Geldin Mesajını Ayarlar.**
-<a:pirate:749380925619437619> ** | **__**${prefix}SAYAÇ-BB-MESAJ**__ **| Sunucunuzdaki Sayacın Görüşürüz Mesajını Ayarlar.**`)
+<a:pirate:749380925619437619> ** | **__**${prefix}DAVET-KANAL**__ **| Sunucunuzda Kaç Kişinin Ne Kadar Daveti Olduğunu Gösteren Kanalı Ayarlar.**
+<a:pirate:749380925619437619> ** | **__**${prefix}DAVET-KANAL-SIFIRLA**__ **| Sunucunuzda Kaç Kişinin Ne Kadar Daveti Olduğunu Gösteren Kanalı Sıfırlar.**
+<a:pirate:749380925619437619> ** | **__**${prefix}DAVET-EKLE**__ **| Etiketlediğiniz Kişinin Davet Sayısını Arttırır.**
+<a:pirate:749380925619437619> ** | **__**${prefix}DAVETLERİM**__ **| Sunucuda Kaç Davetiniz Olduğunu Gösterir.**
+<a:pirate:749380925619437619> ** | **__**${prefix}DAVET-RÜTBE-EKLE**__ **| Sunucunuzda Davet Sayısına Göre Rol Verme Sistemine Rol Ekler.**
+<a:pirate:749380925619437619> ** | **__**${prefix}DAVET-RÜTBE-SİL**__ **| Sunucunuzda Davet Sayısına Göre Rol Verme Sisteminden Rol Siler.**`)
 .addField('<a:pirate:749380925619437619> | Pirate Bot',`  [Botumuza Oy Ver](https://top.gg/bot/713713727794446397) **|**  [Botu Davet Et](https://discord.com/oauth2/authorize?client_id=713713727794446397&scope=bot&permissions=8) **|** [Destek](https://discord.gg/HRC9Yyp)`)
   .setImage(`https://i.hizliresim.com/4Q3agT.jpg`)
 .setThumbnail(client.user.avatarURL)
---
+++ komutlar/yardım-kayıtsistemi.js
@@ -0,0 +1,26 @@
+ const db = require("quick.db");
+const Discord = require('discord.js');
+const fynx = require("../ayarlar/bot.json");
+exports.run = async (client, message, args) => { 
+let prefix = await db.fetch(`prefix.${message.guild.id}`) || fynx.prefix 
+let eklenti = new Discord.MessageEmbed()  
+.setAuthor(`Pirate Yardım Menüsü`)
+.setColor('#ffd100')
+.addField('Sunuzunuzu 7/24 Denetleyin',`
+<a:pirate:749380925619437619> ** | **__**${prefix}KAYIT-KANAL-AYARLA**__ **| Kayıt Ol Komutunun Kullanılacağı Kanalı Belirler.**
+<a:pirate:749380925619437619> ** | **__**${prefix}KAYIT-LOG-KANAL-AYARLA**__ **| Kayıt Ol Komutunun Denetim Kaydı Kanalını Belirler.**
+<a:pirate:749380925619437619> ** | **__**${prefix}KAYIT-OL**__ **| Kayıt Kanalında Kayıt Olmanızı Sağlar.**
+<a:pirate:749380925619437619> ** | **__**${prefix}KAYIT-VERİLECEK-ROL-AYARLA**__ **| Kayıt Ol Komutunu Kullandıktan Sonra Verilecek Rolü Belirler.**
+<a:pirate:749380925619437619> ** | **__**${prefix}KAYIT-ALINACAK-ROL-AYARLA**__ **| Kayıt Ol Komutunu Kullandıktan Sonra Alınacak Rolü Belirler.**
+<a:pirate:749380925619437619> ** | **__**${prefix}GİRİŞ-SİSTEMİ**__ **| Kayıt Kanalına Atılacak Mesajı Ayarlar.**
+<a:pirate:749380925619437619> ** | **__**${prefix}İSİM-SİSTEM**__ **| Kayıt Ol Komutunu Kullandıktan Sonra Verilecek İsmi Belirler.**
+<a:pirate:749380925619437619> ** | **__**${prefix}KAYIT-SİSTEMİ-KAPAT**__ **| Sunucunuzda Davet Sayısına Göre Rol Verme Sisteminden Rol Siler.**`)
+.addField('<a:pirate:749380925619437619> | Pirate Bot',`  [Botumuza Oy Ver](https://top.gg/bot/713713727794446397) **|**  [Botu Davet Et](https://discord.com/oauth2/authorize?client_id=713713727794446397&scope=bot&permissions=8) **|** [Destek](https://discord.gg/HRC9Yyp)`)
+  .setImage(`https://i.hizliresim.com/4Q3agT.jpg`)
+.setThumbnail(client.user.avatarURL)
+ message.channel.send(eklenti) 
+  };
+exports.config = {
+name: "kayıtsistemi",
+  aliases: []
+}
