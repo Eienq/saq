abe4e34ea3f3f28ebc06cd3658af9104cc97bf86
Glitch (fynxmusicbots) <none>
2020-08-30 17:47:25 +0000

    🎃🌌 Checkpoint ./komutlar/davet-ekle.js:2344545/1841 ./komutlar/davet-kanal.js:2344545/1530 ./komutlar/davet-sıfırla.js:2344545/2183 ./komutlar/davetlerim.js:2344545/2947

---
+++ komutlar/davet-ekle.js
@@ -0,0 +1,41 @@
+const Discord = require("discord.js");
+const db = require("quick.db");
+module.exports.run = async (bot, message, args) => {
+  let prefix = (await db.fetch(`prefix.${message.guild.id}`)) || "-";
+  if (!message.member.hasPermission("KICK_MEMBERS")) {
+    const embed = new Discord.MessageEmbed()
+      .setDescription("Ne yazık ki bu komutu kullanmaya yetkin yok. Yönetici yetkisine sahip olmanız gerekmektedir.")
+    .setFooter(bot.user.username, bot.user.avatarURL)
+      .setColor("RED");
+    message.channel.send(embed);
+    return;
+  }
+  let u = message.mentions.users.first();
+let m = args.slice(1).join(" ")
+  if (!u) {
+    return message.channel.send(
+      new Discord.MessageEmbed()
+      .setFooter(bot.user.username, bot.user.avatarURL)
+        .setDescription("Lütfen davet eklenecek kişiyi etiketleyiniz!")
+        .setColor("RED")
+    );
+  }
+    if (!m) {
+    return message.channel.send(
+      new Discord.MessageEmbed()
+      .setFooter(bot.user.username, bot.user.avatarURL)
+        .setDescription("Lütfen eklenecek davet sayısını giriniz.")
+        .setColor("RED")
+    );
+  }
+  const embed = new Discord.MessageEmbed()
+    .setColor("BLACK")
+  .setFooter(bot.user.username, bot.user.avatarURL)
+    .setDescription(`${u} Adlı şahsa; ${m} davet eklendi!`);
+  message.channel.send(embed);
+  db.add(`davet_${u.id}_${message.guild.id}`, +m);
+};
+module.exports.config = {
+    name: "davet-ekle",
+  aliases: ["davetekle"]
+};
---
+++ komutlar/davet-kanal.js
@@ -0,0 +1,33 @@
+const Discord = require("discord.js");
+const db = require("quick.db");
+module.exports.run = async (bot, message, args) => {
+  let prefix = (await db.fetch(`prefix_${message.guild.id}`)) || "!";
+  if (!message.member.hasPermission("KICK_MEMBERS")) {
+    const embed = new Discord.MessageEmbed()
+      .setDescription("Ne yazık ki bu komutu kullanmaya yetkin yok. Yönetici yetkisine sahip olmanız gerekmektedir.")
+    .setFooter(bot.user.username, bot.user.avatarURL)
+      .setColor("RED");
+    message.channel.send(embed);
+    return;
+  }
+  let kanal = message.mentions.channels.first();
+  if (!kanal) {
+    return message.channel.send(
+      new Discord.MessageEmbed()
+        .setDescription("Lütfen bir kanal belirtiniz!")
+      .setFooter(bot.user.username, bot.user.avatarURL)
+        .setColor("RED")
+    );
+  }
+  const embed = new Discord.MessageEmbed()
+    .setColor("GREEN")
+  .setFooter(bot.user.username, bot.user.avatarURL)
+    .setDescription(`Davet kanalı; ${kanal} olarak ayarlandı!`);
+  message.channel.send(embed);
+  db.set(`davetkanal_${message.guild.id}`, kanal.id);
+};
+module.exports.config = {
+  name: "davet-kanal",
+  aliases: ["davetkanal"]
+};
+
