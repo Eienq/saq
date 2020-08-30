2632dc3dd791db66d65c301023e459f61696a8fb
Glitch (fynxmusicbots) <none>
2020-08-30 00:04:08 +0000

    👛👣 Checkpoint ./komutlar/bot-istatistik.js:20905456/12015 ./komutlar/müzik-şuanda-çalan.js:20905456/186

--- komutlar/bot-istatistik.js
+++ komutlar/bot-istatistik.js
@@ -1,3 +1,6 @@
+
+
+
 const Discord = require('discord.js');
 const moment = require('moment');
 const ayarlar = require('../ayarlar/bot.json');
@@ -5,15 +8,14 @@ require('moment-duration-format');
 exports.run = async(client, message, args) => {
 
 let batuhan = new Discord.MessageEmbed()
-.setThumbnail(message.author.displayAvatarURL())
-.setAuthor(client.user.username, client.user.avatarURL)
-.addField("Bot Verileri", `<a:pirate:749380925619437619> **Toplam Sunucu** **|**  **${client.guilds.cache.size}** \n <a:pirate:749380925619437619> Toplam kullanıcı: **${client.guilds.cache.reduce((a, b) => a + b.memberCount, 0).toLocaleString()}** \nToplam kanal: **${client.channels.cache.size}**`)
-.addField("Bot Geliştiricisi", `Bot geliştiricisi ➡ <@424512092742352897> | **' ֎Batuhan#0008** \n Bot geliştiricisi ➡ <@618860034499149859> | **' Miran#0139**`)
-.addField("Sürümler", `Discord.js sürümü: **v${Discord.version}** \nNode.js sürümü: **${process.version}**`)
-.addField("Gecikmeler", `Bot pingi: **${client.ws.ping}** \nMesaj gecikmesi: **${new Date().getTime() - message.createdTimestamp}**`)
-
+.setThumbnail(`https://i.hizliresim.com/4Q3agT.jpg`)
+.addField("__**Bot Verileri**__", `<a:pirate:749380925619437619> **Toplam Sunucu** **|**  **${client.guilds.cache.size}** \n <a:pirate:749380925619437619> **Toplam Kullanıcı** **|** **${client.guilds.cache.reduce((a, b) => a + b.memberCount, 0).toLocaleString()}** \n <a:pirate:749380925619437619> **Toplam Kanal** **|** **${client.channels.cache.size}**`)
+.addField("__**Bot Geliştiricisi**__", `<a:pirate:749380925619437619> **Bot Sahibi**  <@236173144300191754> **|** **Lord Creative#0001** \n <a:pirate:749380925619437619> **Bot geliştiricisi**  <@327064201245753344> **|** **Bay Ördekcik#0001**`)
+.addField("__**Sürümler**__", `<a:pirate:749380925619437619> **Discord.js Sürümü** **|**  **v${Discord.version}** \n<a:pirate:749380925619437619> **Node.js Sürümü** **|**  **${process.version}**`)
+.addField("__**Gecikmeler**__", `<a:pirate:749380925619437619> **Bot pingi** **|**  **${client.ws.ping}** \n <a:pirate:749380925619437619> **Mesaj Gecikmesi** **|** **${new Date().getTime() - message.createdTimestamp}**`)
+.addField("<a:pirate:749380925619437619> **Müzik Oynatılan Sunucu Sayısı**", client.voice.connections.size, true)
 .setTimestamp()
-.setColor("RANDOM")
+.setColor("#ffd100")
 message.channel.send(batuhan)
 }
 
