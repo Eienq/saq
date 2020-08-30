bbdc8173f0a50ba66f25b9795e237427a179e2c3
Glitch (mint-trite-buckthorn) <none>
2020-08-30 22:23:03 +0000

    🍠🏯 Checkpoint ./komutlar/yardım-otorol.js:2344545/1568 ./komutlar/yardım-otocevap.js:2344545/1559 ./komutlar/yardım-davet.js :2344545/1203

---
+++ komutlar/yardım-davetsistemi.js
@@ -0,0 +1,21 @@
+ const db = require("quick.db");
+const Discord = require('discord.js');
+const fynx = require("../ayarlar/bot.json");
+exports.run = async (client, message, args) => { 
+let prefix = await db.fetch(`prefix.${message.guild.id}`) || fynx.prefix 
+let eklenti = new Discord.MessageEmbed()  
+.setAuthor(`Pirate Yardım Menüsü`)
+.setColor('#ffd100')
+.addField('Sunuzunuzu 7/24 Denetleyin',`
+<a:pirate:749380925619437619> ** | **__**${prefix}SAYAÇ-AYARLA**__ **| Sunucunuzda Sayaç Ayarlar.**
+<a:pirate:749380925619437619> ** | **__**${prefix}SAYAÇ-HG-MESAJ**__ **| Sunucunuzdaki Sayacın Hoş Geldin Mesajını Ayarlar.**
+<a:pirate:749380925619437619> ** | **__**${prefix}SAYAÇ-BB-MESAJ**__ **| Sunucunuzdaki Sayacın Görüşürüz Mesajını Ayarlar.**`)
+.addField('<a:pirate:749380925619437619> | Pirate Bot',`  [Botumuza Oy Ver](https://top.gg/bot/713713727794446397) **|**  [Botu Davet Et](https://discord.com/oauth2/authorize?client_id=713713727794446397&scope=bot&permissions=8) **|** [Destek](https://discord.gg/HRC9Yyp)`)
+  .setImage(`https://i.hizliresim.com/4Q3agT.jpg`)
+.setThumbnail(client.user.avatarURL)
+ message.channel.send(eklenti) 
+  };
+exports.config = {
+name: "davetsistemi",
+  aliases: []
+}
---
+++ komutlar/yardım-otorol.js
@@ -0,0 +1,22 @@
+ const db = require("quick.db");
+const Discord = require('discord.js');
+const fynx = require("../ayarlar/bot.json");
+exports.run = async (client, message, args) => { 
+let prefix = await db.fetch(`prefix.${message.guild.id}`) || fynx.prefix 
+let eklenti = new Discord.MessageEmbed()  
+.setAuthor(`Pirate Yardım Menüsü`)
+.setColor('#ffd100')
+.addField('Sunuzunuzu 7/24 Denetleyin',`
+<a:pirate:749380925619437619> ** | **__**${prefix}OTOROL-AYARLA**__ **| Sunucunuzda Otorol Ayarlar.**
+<a:pirate:749380925619437619> ** | **__**${prefix}OTOROL-KAPAT**__ **| Sunucunuzdaki Otorol'ü Kapatır.**
+<a:pirate:749380925619437619> ** | **__**${prefix}OTOROL-MESAJ**__ **| Sunucunuzdaki Otorol'ün Mesajını Ayarlar.**
+<a:pirate:749380925619437619> ** | **__**${prefix}OTOROL-MESAJ-SIFIRLA**__ **| Sunucunuzdaki Otorol'ün Mesajını Sıfırlar.**`)
+.addField('<a:pirate:749380925619437619> | Pirate Bot',`  [Botumuza Oy Ver](https://top.gg/bot/713713727794446397) **|**  [Botu Davet Et](https://discord.com/oauth2/authorize?client_id=713713727794446397&scope=bot&permissions=8) **|** [Destek](https://discord.gg/HRC9Yyp)`)
+  .setImage(`https://i.hizliresim.com/4Q3agT.jpg`)
+.setThumbnail(client.user.avatarURL)
+ message.channel.send(eklenti) 
+  };
+exports.config = {
+name: "otorol",
+  aliases: []
+}
