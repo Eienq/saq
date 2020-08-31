35a0780059147f8fa1d08d190a8a934c0ff0e19f
Glitch (mint-trite-buckthorn) <none>
2020-08-31 22:24:55 +0000

    🛫🏉 Checkpoint ./komutlar/zjsrol-fynxcode.js:20905456/72 ./package.json:20905456/10 ./komutlar/bot-shard.js:2344545/433:20905456/976 ./komutlar/yardım-genel.js:20905456/198 ./komutlar/bot-istatistik.js:2344545/189:20905456/69

--- komutlar/bot-shard.js
+++ komutlar/bot-shard.js
@@ -18,12 +18,12 @@ let shardinfo = {
     }
 let i = client.shard.id
     let shardembed = new Discord.MessageEmbed()
-    .setTitle('Shard bilgi/İstatistik')
-    .setFooter('Bu Sunucunun Shardı İdsi: ' + client.shard.id + '')
-    .setColor(ayarlar.token)
+    .setTitle('Shard İstatistik')
+    .setColor("#ffd100")
+    .setImage(`https://i.hizliresim.com/Y7jFCB.png`)
     
     for(i=0;i<client.shard.count;i++) {
-        shardembed.addField(`🔹**Shard ${i}**`, `▫️ **Ping:** ${Math.round(shardinfo.ping[i])}ms\n▫️ **Sunucu:** ${shardinfo.server_count[i]}\n▫️ **Kullanıcı:** ${shardinfo.user_count[i]}\n▫️ **Uptime:** ${moment.duration(shardinfo.uptime[i]).format(`D [Gün] , H [Saat], m [Dakika], s [Saniye]`)} `, true)
+        shardembed.addField(`<a:hypesquad1:750076071721828452> **Shard ${i}**`, `<a:hypesquad1:750076071721828452>  **Ping:** ${Math.round(shardinfo.ping[i])}ms\n<a:hypesquad1:750076071721828452>  **Sunucu:** ${shardinfo.server_count[i]}\n <a:hypesquad1:750076071721828452> **Kullanıcı:** ${shardinfo.user_count[i]}\n <a:hypesquad1:750076071721828452> **Uptime:** ${moment.duration(shardinfo.uptime[i]).format(`D [Gün] , H [Saat], m [Dakika], s [Saniye]`)} \n <a:hypesquad1:750076071721828452> **Bu Sunucunun Shardı Id'si**: ${client.shard.id}`, true)
     }
     message.channel.send(shardembed)
 }
--- komutlar/zjsrol-fynxcode.js
+++ komutlar/zjsrol-fynxcode.js
@@ -6,10 +6,10 @@ const dbl = new DBL('DBL TOKEN', client);
 exports.run = (client, message) => {
     dbl.hasVoted(message.author.id).then(voted => {
         if (!voted) {
-            message.reply("**Bu Komutu Kullanabilmek İçin Top.gg Üzerinden Oy Vermen Gerekiyor**\n**Oy vermek için;**\n > https://top.gg/bot/713713727794446397 | Eğer oy verdiyseniz __3__ dakika bekleyiniz.") //OY LINKI!
+            message.reply("<a:hypesquad1:750076071721828452> **Bu Komutu Kullanabilmek İçin Top.gg Üzerinden Oy Vermen Gerekiyor**\n**Oy vermek için;**\n > https://top.gg/bot/713713727794446397 | Eğer oy verdiyseniz __3__ dakika bekleyiniz.") //OY LINKI!
 
         } else {
-            message.channel.send("JavaScript Rolün Verildi");
+            message.channel.send("<a:hypesquad1:750076071721828452> JavaScript Rolün Verildi");
             message.member.roles.add("729326539572772875")  
 
         }
