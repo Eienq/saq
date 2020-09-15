419301611dfe7f8d9fb1a28189298e110eeafd67
Glitch (mint-trite-buckthorn) <none>
2020-09-15 14:24:42 +0000

    📅😜 Checkpoint ./komutlar/yardım-guard.js:20905456/477 ./komutlar/yardım-müzik.js:20905456/488

--- komutlar/yardım-müzik.js
+++ komutlar/yardım-müzik.js
@@ -3,10 +3,6 @@ const Discord = require('discord.js');
 const fynx = require("../ayarlar/bot.json");
 exports.run = async (client, message, args) => { 
 let prefix = await db.fetch(`prefix.${message.guild.id}`) || fynx.prefix 
-  const fynxdblapi = require('dblapi.js')
-const fynxdbl = new fynxdblapi(fynx.dbltoken, client) 
-fynxdbl.hasVoted(message.author.id).then(fynxoyverdi => {
-      if(fynxoyverdi)  {
 let eklenti = new Discord.MessageEmbed()  
 .setColor('#ffd100') 
 .setAuthor(`Pirate Müzik Komutları`, client.user.avatarURL())
@@ -25,9 +21,6 @@ let eklenti = new Discord.MessageEmbed()
 .setImage(`https://i.hizliresim.com/Y7jFCB.png`)
 .setThumbnail(client.user.avatarURL)
  message.channel.send(eklenti) 
-                     } else {
-        message.channel.send("Bu komutu kullanabilmek için bota oy vermeniz gerekmektedir. Bota oy verebilmek için https://discordbots.org/bot/713713727794446397/vote sitesini ziyaret edebilirsiniz. | Oy verdiyseniz 3 dakika beklemeniz gerekmektedir.")
-                     }})
   };
 exports.config = {
 name: "müzik",
